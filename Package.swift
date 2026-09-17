300b59c16526a9e5e7a733c8874f832e2d4fa745
Universal Echo <cartisim@icloud.com>
2026-09-17 10:29:39 +0800

    Using Vapor 5

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.0
+// swift-tools-version:6.4
 import PackageDescription
 
 let package = Package(
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.1.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.110.2"),
+        .package(url: "https://github.com/vapor/vapor.git", exact: "5.0.0-beta.2"),
     ],
     targets: [
         .target(
